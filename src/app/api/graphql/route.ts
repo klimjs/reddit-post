d9714e0d40260045f582d6b7a3263c2358aed89f
Klim Semenov <klimjsdev@gmail.com>
2025-11-02 16:59:20 +0100

    type mismatch fix

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -24,4 +24,10 @@ const handler = startServerAndCreateNextHandler<NextRequest>(server as any, {
   context: async (req) => ({ req }),
 })
 
-export { handler as GET, handler as POST }
+export async function GET(request: NextRequest) {
+  return handler(request)
+}
+
+export async function POST(request: NextRequest) {
+  return handler(request)
+}
